859c71dde43a7c7156f57dbf84d15442b8fd8ae3
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2024-09-02 23:47:42 +0530

    conditions

--- class.cs
+++ class.cs
@@ -8,24 +8,35 @@ int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if (total > 14)
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You win!");
-}
-if (total < 15)
-{
-    Console.WriteLine("You lose!");
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+
+    Console.WriteLine($"Your total including the bonus: {total}");
 }
 
-string message = "The quick brown fox jumps over the lazy dog.";
-bool result = message.Contains("dog");
-Console.WriteLine(result);
-
-if (message.Contains("fox"))
+if (total >= 16)
 {
-    Console.WriteLine("What does the fox say?");
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
 }
 else
 {
-    Console.WriteLine("No fox found.");
+    Console.WriteLine("You win a kitten!");
 }
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43246d653e22203f5a391d1bdd566d40891fb8a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac3417a512d62c51ee0bf2420edf7e5195443eca")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
