43246d653e22203f5a391d1bdd566d40891fb8a9
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2024-09-02 22:57:27 +0530

    first commit

---
+++ class.cs
@@ -0,0 +1,18 @@
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if (total > 14)
+{
+    Console.WriteLine("You win!");
+}
+if (total < 15)
+{
+    Console.WriteLine("You lose!");
+}
