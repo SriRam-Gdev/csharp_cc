7d19273d54a3dc6e3cdeb7ffa5f616785dad7be5
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2024-09-03 12:32:05 +0530

    string

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// string[] fraudulentOrderIDs = new string[3];
+
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+// //fraudulentOrderIDs[3] = "D000";
+
+string[] fraudulentOrderIDs = [ "A123", "B456", "C789" ];
+
+Console.WriteLine($"first = {fraudulentOrderIDs[0]}");
+Console.WriteLine($"second = {fraudulentOrderIDs[1]}");
+Console.WriteLine($"third = {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
