212082097816a7ffb72e6648272048a2cc71065e
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2024-09-03 11:41:38 +0530

    z

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+859c71dde43a7c7156f57dbf84d15442b8fd8ae3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3c860135bd872c38b26fa1881893b13d9e635bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
